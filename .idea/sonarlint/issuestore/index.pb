62e9692f4e5479b9beb14c7c84d0a6c8ce300247
Black-Bong <shoy1415@naver.com>
2021-12-21 11:34:24 +0900

    백준_그대로 출력하기_11718

--- .idea/sonarlint/issuestore/index.pb
+++ .idea/sonarlint/issuestore/index.pb
@@ -1,6 +1,3 @@
 
 =
 algorithm.iml,1/6/1648eeac5c90967ac9aba4bdea7c72a8ab9f970f
-:
-
-.gitignore,a/5/a5cc2925ca8258af241be7e5b0381edf30266302
---
+++ src/baekjoon/Bjo11718.java
@@ -0,0 +1,14 @@
+package baekjoon;
+
+import java.util.Scanner;
+
+public class Bjo11718 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        while (scanner.hasNext()) {
+            String s = scanner.nextLine();
+            System.out.println(s);
+        }
+    }
+}
