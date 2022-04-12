fe9e72561c83032a2d3a9ea84b6c9d3536ae0f9e
geombong <shoy1415@gmail.com>
2022-04-12 12:35:27 +0900

    ETC_최대 매출

---
+++ src/etc/maximumSales.java
@@ -0,0 +1,55 @@
+package etc;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class maximumSales {
+    static class Main {
+        public int solution(int days, int[] salesByDate) {
+            int answer = 0;
+            int maximumSales = 0;
+            int slideCount = days;
+            for (int i = 0; i < salesByDate.length - (days - 1); i++) {
+                for (int j = i; j < slideCount; j++) {
+                    maximumSales += salesByDate[j];
+                }
+                if (answer < maximumSales) {
+                    answer = maximumSales;
+                }
+                slideCount++;
+                maximumSales = 0;
+            }
+            return answer;
+        }
+
+        public static void main(String[] args) throws IOException {
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+            BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(System.out));
+            StringTokenizer stringTokenizer = new StringTokenizer(bufferedReader.readLine(), " ");
+
+            int date = Integer.parseInt(stringTokenizer.nextToken());
+            int days = Integer.parseInt(stringTokenizer.nextToken());
+
+            int[] salesByDate = new int[date];
+            stringTokenizer = new StringTokenizer(bufferedReader.readLine(), " ");
+
+            for (int i = 0; i < date; i++) {
+                salesByDate[i] = Integer.parseInt(stringTokenizer.nextToken());
+            }
+
+            Main main = new Main();
+
+            int result = main.solution(days, salesByDate);
+
+            bufferedReader.close();
+
+            bufferedWriter.write(String.valueOf(result));
+            bufferedWriter.flush();
+            bufferedWriter.close();
+        }
+    }
+}
